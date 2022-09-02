513175eb8561f5fc7f1c75ea1b3b5ae720de1e94
jina <pja101010@gmail.com>
2022-09-02 15:36:29 +0900

    JUnit 5 시작하기

--- src/test/java/me/whiwteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiwteship/inflearnthejavatest/StudyTest.java
@@ -10,12 +10,39 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
     void create_new_study() {
-        Study study = new Study(-10);
+       Study study = new Study(-10);
+
         assertNotNull(study);
+        //assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 DRAFT 상태다.");
+        //람다식을 사용하여 메세지 출력시 더 효율 비용이 좋다.
         assertEquals(StudyStatus.DRAFT, study.getStatus(),
                 () -> "스터디를 처음 만들면" + StudyStatus.DRAFT + "상태다.");
         //assertTrue(1 < 2);
         assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.");
+
+    }
+
+    @Test
+    @DisplayName("한꺼번에 오류를 잡을 수 있다.")
+    void create_new_study2() {
+        Study study = new Study(-10);
+
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        () -> "스터디를 처음 만들면" + StudyStatus.DRAFT + "상태다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+        );
+    }
+
+    @Test
+    @DisplayName("특정 throw를 던질 수 있다.")
+    void create_new_study3() {
+        //특정 오류를 발생시킬 수 있다.
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = exception.getMessage();
+        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
     }
 
     @Test
